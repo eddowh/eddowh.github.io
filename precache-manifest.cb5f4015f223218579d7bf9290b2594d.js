5c120bac1ee84c435e6d77c5253d5529a572654c
Eddo W. Hintoso <eddo.hintoso+github@gmail.com>
2019-06-19 22:42:29 -0700

    Initial publish

---
+++ precache-manifest.cb5f4015f223218579d7bf9290b2594d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4af06220c49f92c5c2ee",
+    "url": "/css/app.0bb047ce.css"
+  },
+  {
+    "revision": "4af06220c49f92c5c2ee",
+    "url": "/js/app.85764518.js"
+  },
+  {
+    "revision": "f7161f8be9b82ed7ca8f",
+    "url": "/js/chunk-vendors.4dcbfa82.js"
+  },
+  {
+    "revision": "dc85b98508ab07e47d850df250db16d1",
+    "url": "/img/eddo-logo-recentered.dc85b985.png"
+  },
+  {
+    "revision": "07873e92b85532ecf7e1d1962138aec8",
+    "url": "/img/rd-screenshot.07873e92.png"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "92068e1844e78242db07751c22459096",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
---
+++ precache-manifest.e8bee44e7a20d16276281a56baf35b4c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "873242a0e8ec834df866",
+    "url": "/css/app.0136e619.css"
+  },
+  {
+    "revision": "873242a0e8ec834df866",
+    "url": "/js/app.f10531da.js"
+  },
+  {
+    "revision": "f7161f8be9b82ed7ca8f",
+    "url": "/js/chunk-vendors.4dcbfa82.js"
+  },
+  {
+    "revision": "dc85b98508ab07e47d850df250db16d1",
+    "url": "/img/eddo-logo-recentered.dc85b985.png"
+  },
+  {
+    "revision": "07873e92b85532ecf7e1d1962138aec8",
+    "url": "/img/rd-screenshot.07873e92.png"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "defbe3bcbc90fec3df2539b04594ae0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
